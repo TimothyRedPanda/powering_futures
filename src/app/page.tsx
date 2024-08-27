e3dd13ce4aebbcf97b9451932423f9812ae0741b
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-08-27 16:16:21 +0100

    adding ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 import askQuestion from "./lib/fetch";
 import Image from "next/image";
+import { useState } from "react";
 // import pfLogo from "../PowerFuture.png";
 
 export default function Home() {
+  const [reply, setReply] = useState("");
+
   async function triggerQuestion() {
-    console.log("Triggering question...");
+    console.log("Triggering question…");
+    setReply("Analysing…");
     try {
       const response = await askQuestion(
-        "What are the entry-level positions in companies across different industries?"
+        "Hey im bob can you help me find a job?"
       );
       console.log("Response received:", response);
+      setReply(response.choices[0].message.content);
     } catch (error) {
       console.error("Error in triggerQuestion:", error);
     }
   }
   return (
-    <main className="grid h-screen w-screen grid-rows-custom">
-      <nav className=" bg-fp-green w-screen h-fit p-4">
+    <main className="grid h-screen w-screen grid-rows-custom bg-fp-green text-slate-100">
+      <nav className="  w-screen h-fit p-4">
         <Image
           src="/PowerFuture.png"
           alt="Powering Futures Logo"
@@ -25,8 +30,11 @@ export default function Home() {
           width={160}
         />
       </nav>
-      <section className="p-4 flex flex-col gap-2 items-center">
+      <section className="p-4 flex flex-col gap-2 items-center h-1/2 justify-between">
         <h1>Powering Futures AI</h1>
+        <div className="w-1/2">
+          <p className=" text-center">{reply}</p>
+        </div>
         <button
           className="h-fit w-fit px-4 py-2 bg-fp-gold text-slate-50 rounded-md"
           type="button"
