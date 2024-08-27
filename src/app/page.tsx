a053c84af64406634510e61aefaff1b38a922a43
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-08-27 12:48:27 +0100

    Page styling set-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,23 @@ export default function Home() {
 				"What are the entry-level positions in companies across different industries?",
 			);
 			console.log("Response received:", response);
-			// Handle the response here, e.g., display it in the UI
 		} catch (error) {
 			console.error("Error in triggerQuestion:", error);
 		}
 	}
 	return (
-		<main className="grid place-content-center p-4 gap-4">
-			<h1>Powering Futures AI</h1>
-			<button
-				className="px-4 py-2 bg-green-900 text-slate-50 rounded-md"
-				type="button"
-				onClick={() => triggerQuestion()}
-			>
-				Ask questions
-			</button>
+		<main className="grid h-screen w-screen grid-rows-custom">
+			<nav className=" bg-fp-green w-screen h-20 p-4">Hello</nav>
+			<section className="p-4 flex flex-col gap-2 items-center">
+				<h1>Powering Futures AI</h1>
+				<button
+					className="h-fit w-fit px-4 py-2 bg-fp-gold text-slate-50 rounded-md"
+					type="button"
+					onClick={() => triggerQuestion()}
+				>
+					Ask questions
+				</button>
+			</section>
 		</main>
 	);
 }
